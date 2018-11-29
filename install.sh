dde3947ad20f13cc0417abe52fe301544c3a25f5
Kaylan Horne <Kaylan.horne3@gmail.com>
2018-11-29 03:56:37 +0000

    Finished automation

--- install.sh
+++ install.sh
@@ -7,21 +7,10 @@ sudo curl --fail -sSLo /etc/yum.repos.d/passenger.repo https://oss-binaries.phus
 sudo yum install -y mod_passenger || sudo yum-config-manager --enable cr && sudo yum install -y mod_passenger
 sudo service httpd restart
 sudo mkdir /var/www/lab8
-**clone contents** cp -a * /var/www/lab8
-sudo nano /etc/httpd/conf.d/myapp.conf
-<VirtualHost *:80>
-    DocumentRoot /var/www/lab8/
-    PassengerAppRoot /var/www/lab8/
-    PassengerAppType wsgi
-    PassengerStartupFile passenger_wsgi.py
-    
-    <Directory /var/www/myapp>
-      Allow from all
-      Options -MultiViews
-      Require all granted
-    </Directory>
-</VirtualHost>
-sudo service httpd restart
+cp -a * /var/www/lab8
+cp myapp.conf /etc/httpd/conf.d/myapp.conf
+service httpd restart
+
 
 
 
